1bf2d8c01eb5629a618af7ce3ad11eaa9e21c6cd
uglydavy <aknk.v@protonmail.ch>
2022-09-01 11:12:46 +0800

    Initial Commit

---
+++ InformationManagement/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  InformationManagement
+//
+//  Created by 𝔲𝔤𝔩𝔶 ♡ on 9/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InformationManagement/InformationManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  InformationManagementApp.swift
+//  InformationManagement
+//
+//  Created by 𝔲𝔤𝔩𝔶 ♡ on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct InformationManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
